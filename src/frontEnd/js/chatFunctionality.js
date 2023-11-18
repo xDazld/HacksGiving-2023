5fb3f7e314e0f103c3e2ff1038d831dba5355072
Alex-J-Lopez <115580820+Alex-J-Lopez@users.noreply.github.com>
2023-11-18 13:58:09 -0600

    Removed the loading symbol
    Didn't look right and had bugs

--- src/frontEnd/js/chatFunctionality.js
+++ src/frontEnd/js/chatFunctionality.js
@@ -2,20 +2,15 @@ const socket = io({
     path: "/HacksGiving-2023/socket.io",
 });
 
-let loadingSymbol;
 let chatArea;
 
 const displayMessage = (message) => {
-    
-    loadingSymbol.style.visibility = "hidden";
-    
     chatArea.innerHTML += `<div class='generatedResponse'>${message}</div>`;
 };
 
 const sendMessage = () => {
     const message = document.getElementById("userInput").value;
     socket.emit("inputMessage", message);
-    loadingSymbol.style.visibility = "visible";
     document.getElementById("userInput").value = "";
     chatArea.innerHTML += `<div class='userMessage'>${message}</div>`;
 };
@@ -31,7 +26,6 @@ window.onload = () => {
     });
 
     chatArea = document.getElementById("chatArea");
-    loadingSymbol = document.getElementById("LoadingSymbol");
     document.getElementById("navBarTitle").onclick = () => {
         sessionStorage.clear();
         location.replace("/HacksGiving-2023/");
